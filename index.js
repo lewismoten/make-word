c7ed187b8d40003961227502b7194ba97141cadb
Lewis Moten <lewismoten@gmail.com>
2016-07-04 00:02:46 -0400

    Convert to weighted actions.

--- index.js
+++ index.js
@@ -8,17 +8,17 @@
 
     let length = randomNumber(min || 8, max || 8),
       sb = new StringBuilder(),
-      actions = require('./lib/actions');
+      actions = require('./lib/actions'),
+      actionable = getCandidates(actions, sb, length);
 
-    while (actions.some(action => action.canDo(sb, length))) {
+    while (actionable.length > 0) {
 
-      // We can do an action
+      // We can do at least 1 action ... lets choose!
+      let sum = actionable.reduce(sumWeights, 0),
+        action = chooseAction(actionable, sum);
 
-      while(!actions.some(action => action.do(sb, length))) {
-
-        // we didn't do anything yet...
-
-      }
+      action.do(sb, length);
+      actionable = getCandidates(actions, sb, length);
 
     }
 
@@ -26,9 +26,45 @@
 
   };
 
+  function chooseAction(actions, sum) {
+
+    let pivot = Math.random() * sum,
+      chosen;
+
+    actions.some((action) => {
+
+      pivot -= action.weight;
+
+      if (action.weight > pivot) {
+
+        chosen = action;
+        return true;
+
+      }
+
+      return false;
+
+    });
+
+    return chosen;
+
+  }
+
+  function getCandidates(actions, sb, length) {
+
+    return actions.filter(action => action.canDo(sb, length));
+
+  }
+
   function randomNumber(min, max) {
 
     return Math.round(Math.random() * (max - min)) + min;
+
+  }
+
+  function sumWeights(sum, action) {
+
+    return sum + action.weight;
 
   }
 
