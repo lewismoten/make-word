80d04edfca1054105f9dfce45c8d11563ba25717
Lewis Moten <lewismoten@gmail.com>
2016-07-04 02:51:26 -0400

    Run...

---
+++ run.js
@@ -0,0 +1,5 @@
+let lib = require('./lib');
+
+for(let i = 0; i < 40; i++) {
+  console.log(lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8), lib(8, 8));
+}
