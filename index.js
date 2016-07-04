cf92c90b7e453450ef775f739eccccbb2fe0bc32
Lewis Moten <lewismoten@gmail.com>
2016-07-03 21:24:22 -0400

    run beautify plugin

--- index.js
+++ index.js
@@ -4,54 +4,50 @@
 
   module.exports = RandomPassword;
 
-function RandomPassword(Min, Max)
-{
-	var password = new String('');
-	var addConsonant = new Boolean(true);
-	var doubleConsonants = new String('cdfglmnprst');
-	var singleConsonants = new String('bcdfghjklmnprstv');
-	var letter = new String('');
-	var vowels = new String('aeiou');
-	var length = new Number(0);
-	var index = new Number(0);
+  function RandomPassword(Min, Max) {
 
-	length = Math.round(Math.random() * (Max - Min)) + Min;
+      letter = new String(''),
+      vowels = new String('aeiou'),
+      length = new Number(0),
+      index = new Number(0);
 
-	while (password.length < length)
-	{
-		random = Math.round(Math.random() * 100);
+    length = Math.round(Math.random() * (Max - Min)) + Min;
 
-		// 10% Double Consonants
-		if(password != '' && addConsonant && random < 10)
-		{
-			index = Math.round(Math.random() * (doubleConsonants.length - 1));
-			letter = doubleConsonants.substring(index, index+1);
-			password += letter + letter;
-			addConsonant = false;
-		}
-		else
-		{
-			// 80% Consonants
-			if(password != '' && addConsonant && random < 90)
-			{
-				index = Math.round(Math.random() * (singleConsonants.length - 1));
-				letter = singleConsonants.substring(index, index+1);
-				password += letter;
-				addConsonant = false;
-			}
-			// 10% vowels
-			else
-			{
-				index = Math.round(Math.random() * (vowels.length - 1))
-				letter = vowels.substring(index, index+1);
-				password += letter;
-				addConsonant = true;
-			}
-		}
-	}
-	if(password.length > Max)
-		password = password.substring(0, Max)
-	return(password)
-}
+    while (password.length < length)
+    {
+      random = Math.round(Math.random() * 100);
+
+      // 10% Double Consonants
+      if (password != '' && addConsonant && random < 10)
+      {
+        index = Math.round(Math.random() * (doubleConsonants.length - 1));
+        letter = doubleConsonants.substring(index, index + 1);
+        password += letter + letter;
+        addConsonant = false;
+      }
+      else
+      {
+        // 80% Consonants
+        if (password != '' && addConsonant && random < 90)
+        {
+          index = Math.round(Math.random() * (singleConsonants.length - 1));
+          letter = singleConsonants.substring(index, index + 1);
+          password += letter;
+          addConsonant = false;
+        }
+        // 10% vowels
+        else
+        {
+          index = Math.round(Math.random() * (vowels.length - 1))
+          letter = vowels.substring(index, index + 1);
+          password += letter;
+          addConsonant = true;
+        }
+      }
+    }
+    if (password.length > Max)
+      password = password.substring(0, Max)
+    return (password)
+  }
 
 })();
