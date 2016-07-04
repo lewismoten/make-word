5bc05e883e20fc7158fc1b9f82e18860fbef47e7
Lewis Moten <lewismoten@gmail.com>
2016-07-04 00:20:47 -0400

    Test different lengths

--- spec/lib-spec.js
+++ spec/lib-spec.js
@@ -13,6 +13,37 @@
 
     });
 
+    // TODO: Take over Math.random to reduce and control our tests!
+
+    for (let i = 1; i < 32; i++) {
+
+      it(`can be limited to ${i} characters`, () => {
+
+        let result = lib(i, i);
+
+        expect(result.length).toBe(i);
+
+      });
+
+    }
+
+    for (let min = 1; min < 32; min++) {
+
+      for (let max = min + 1; max < 32; max++) {
+
+        it(`can be limited between ${min} and ${max} characters`, () => {
+
+            let result = lib(min, max);
+            expect(result.length).not.toBeLessThan(min);
+            expect(result.length).not.toBeGreaterThan(max);
+
+        });
+
+      }
+
+    }
+
+
     it('has a default length of 8', () => {
 
       let result = lib();
