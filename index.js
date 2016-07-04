0b2d95e923442ccac4f0beb979fd37091df862b1
Lewis Moten <lewismoten@gmail.com>
2016-07-03 21:29:24 -0400

    Delinting complete ...?

--- index.js
+++ index.js
@@ -13,45 +13,52 @@
       letter = '',
       vowels = 'aeiou',
       length = 0,
-      index = 0;
+      index = 0,
+      random;
 
     length = Math.round(Math.random() * (Max - Min)) + Min;
 
-    while (password.length < length)
-    {
+    while (password.length < length) {
+
       random = Math.round(Math.random() * 100);
 
       // 10% Double Consonants
-      if (password != '' && addConsonant && random < 10)
-      {
+      if (password !== '' && addConsonant && random < 10) {
+
         index = Math.round(Math.random() * (doubleConsonants.length - 1));
         letter = doubleConsonants.substring(index, index + 1);
         password += letter + letter;
         addConsonant = false;
-      }
-      else
-      {
+
+      } else if (password !== '' && addConsonant && random < 90) {
+
         // 80% Consonants
-        if (password != '' && addConsonant && random < 90)
-        {
-          index = Math.round(Math.random() * (singleConsonants.length - 1));
-          letter = singleConsonants.substring(index, index + 1);
-          password += letter;
-          addConsonant = false;
-        }
+
+        index = Math.round(Math.random() * (singleConsonants.length - 1));
+        letter = singleConsonants.substring(index, index + 1);
+        password += letter;
+        addConsonant = false;
+
+      } else {
+
         // 10% vowels
-        else
-        {
-          index = Math.round(Math.random() * (vowels.length - 1))
-          letter = vowels.substring(index, index + 1);
-          password += letter;
-          addConsonant = true;
-        }
+        index = Math.round(Math.random() * (vowels.length - 1));
+        letter = vowels.substring(index, index + 1);
+        password += letter;
+        addConsonant = true;
+
       }
+
     }
-    if (password.length > Max)
-      password = password.substring(0, Max)
-    return (password)
+
+    if (password.length > Max) {
+
+      password = password.substring(0, Max);
+
+    }
+
+    return password;
+
   }
 
 })();
